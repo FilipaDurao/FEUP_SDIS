3d6806f0f1627ebe1ca41f089a09253c806ab5bc
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-04-10 17:44:03 +0100

    Fixed bug in TCP restorer

--- src/proj/peer/rmi/FileRestorerTCP.java
+++ src/proj/peer/rmi/FileRestorerTCP.java
@@ -6,15 +6,17 @@ import proj.peer.handlers.async.ChunkInitiatorTCPHandler;
 import proj.peer.message.messages.GetChunkMessage;
 import proj.peer.operations.SaveChunkOperation;
 
+import java.util.concurrent.CountDownLatch;
+
 
 public class FileRestorerTCP extends FileRestorer {
     FileRestorerTCP(Peer peer) {
         super(peer);
     }
 
-    protected void initiateRestoreChunk(Integer chunkNo, String encode, SaveChunkOperation chunkSaver) throws Exception {
+    protected void initiateRestoreChunk(Integer chunkNo, String encode, SaveChunkOperation chunkSaver, CountDownLatch latch) throws Exception {
         GetChunkMessage msg = new GetChunkMessage(this.peer.getVersion(), this.peer.getPeerId(), encode, chunkNo);
-        ChunkInitiatorHandler handler = new ChunkInitiatorTCPHandler(peer, msg, chunkSaver, null);
+        ChunkInitiatorHandler handler = new ChunkInitiatorTCPHandler(peer, msg, chunkSaver, latch);
         this.peer.getRestore().subscribe(handler);
         handler.startAsync();
     }
